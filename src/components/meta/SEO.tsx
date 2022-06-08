10bc00af5a6826c10b1180ee433a80d8c2271bcf
chudoklates <franczalko@gmail.com>
2022-06-08 15:48:10 +0200

    Placeholder index page

---
+++ src/components/meta/SEO.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+type MetaTag = {
+  name?: string
+  content: string
+  property?: string
+}
+
+type SEOProps = {
+  title: string
+  description: string
+  meta?: MetaTag[]
+}
+
+const SEO = ({
+  title,
+  meta,
+  description
+}: SEOProps) => {
+  const defaultMeta:MetaTag[] = [
+    {
+      name: `description`,
+      content: description,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: description,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:title`,
+      content: title,
+    },
+    {
+      name: `twitter:description`,
+      content: description,
+    },
+  ]
+  
+  return (
+    <Helmet
+      title={title}
+      meta={meta ? defaultMeta.concat(meta) : defaultMeta}
+    />
+  )
+}
+
+export default SEO
