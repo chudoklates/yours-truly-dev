7156b9fb75e59d1938aab953769bdc2d9d3ca2fd
chudoklates <franczalko@gmail.com>
2022-06-10 16:39:02 +0200

    Important punctuation

--- src/pages/data-privacy.tsx
+++ src/pages/data-privacy.tsx
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row'
 import SEO from "../components/meta/SEO"
 
 const description:string = `
-  How I handle your data
+  How I handle your data.
 `
 
 const DataPrivacyPage = () => {
