4738ed6f3f9917c86ef8082bfeab4a6148b584ee
chudoklates <franczalko@gmail.com>
2022-10-31 13:31:46 +0100

    Add footer, Font Awesome

--- src/components/main/Layout.tsx
+++ src/components/main/Layout.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Footer';
+import { Stack } from 'react-bootstrap';
 
 type LayoutProps = {
-  children: React.ReactNode
+  children: React.ReactNode;
 };
 
 function Layout({ children }: LayoutProps) {
   return (
-    <main style={{ height: '100%' }}>
-      <Container>
-        {children}
-      </Container>
-    </main>
+    <React.Fragment>
+      <Stack gap={3} style={{ height: '100vh' }}>
+        <main>
+          <Container>{children}</Container>
+        </main>
+        <Footer />
+      </Stack>
+    </React.Fragment>
   );
 }
 
