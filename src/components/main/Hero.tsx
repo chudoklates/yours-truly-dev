51dac1fbac3f2ca271d339dbfd6bcf8f82a0c829
chudoklates <franczalko@gmail.com>
2023-10-19 14:59:28 +0200

    Add Hero component; footer updates

---
+++ src/components/main/Hero.tsx
@@ -0,0 +1,47 @@
+import { StaticImage } from 'gatsby-plugin-image';
+import * as React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Hero = () => {
+  return (
+    <div style={{ display: 'grid', maxHeight: 600 }}>
+      <StaticImage
+        src={`../../images/background.webp`}
+        layout="fullWidth"
+        objectFit="cover"
+        alt=""
+        style={{ gridArea: '1/1' }}
+      />
+      <div
+        style={{ gridArea: '1/1', position: 'relative' }}
+        className="text-uppercase text-white container"
+      >
+        <Row className="h-100">
+          <Col>
+            <Row className="align-items-center h-100 text-center">
+              <p className="display-1 fw-bold opacity-50">IT ME</p>
+              <p className="display-1 fw-bold opacity-75">IT ME</p>
+              <h2 className="display-1 fw-bold">IT ME</h2>
+              <p className="display-1 fw-bold opacity-75">IT ME</p>
+              <p className="display-1 fw-bold opacity-50">IT ME</p>
+            </Row>
+          </Col>
+          <Col>
+            <Row className="align-items-center justify-content-center h-100">
+              <h5 className="bg-dark display-5 bg-opacity-75">
+                I am yours truly,
+                <br />
+                Wojciech Jagielnicki
+                <br />
+                React/Node.js developer
+              </h5>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/data-privacy.tsx
+++ src/pages/data-privacy.tsx
@@ -6,38 +6,40 @@ const description: string = `
   How I handle your data.
 `;
 
-function DataPrivacyPage() {
+const DataPrivacyPage = () => {
   return (
     <>
-      <SEO title="Yours Truly, Dev" description={description} />
-      <Row
-        style={{
-          margin: '5vh 0',
-        }}
-        className="text-uppercase"
-      >
-        <h1>Data Policy:</h1>
-        <h3 className="text-">I vow to: </h3>
-        <div>
-          <ul>
-            <li>Not share your data with anybody</li>
-            <li>Not collect your full IP address</li>
-          </ul>
-        </div>
-        <h3>What I will know: </h3>
-        <div>
-          <ul>
-            <li>Your device properties (system, screen size)</li>
-            <li>Your interactions with the website</li>
-            <li>
-              Your approximate location in the world (based on an anonymised IP
-              address)
-            </li>
-          </ul>
-        </div>
-      </Row>
+      <SEO title="Data Privacy" description={description} />
+      <main className="container">
+        <Row
+          style={{
+            margin: '5vh 0',
+          }}
+          className="text-uppercase"
+        >
+          <h1>Data Policy:</h1>
+          <h3 className="text-">I vow to: </h3>
+          <div>
+            <ul>
+              <li>Not share your data with anybody</li>
+              <li>Not collect your full IP address</li>
+            </ul>
+          </div>
+          <h3>What I will know: </h3>
+          <div>
+            <ul>
+              <li>Your device properties (system, screen size)</li>
+              <li>Your interactions with the website</li>
+              <li>
+                Your approximate location in the world (based on an anonymised
+                IP address)
+              </li>
+            </ul>
+          </div>
+        </Row>
+      </main>
     </>
   );
-}
+};
 
 export default DataPrivacyPage;
